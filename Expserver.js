79b03cac20fef6be1973a16dee1f352720b71713
Sanyam Sharma <106426986+sanyamsharmaa@users.noreply.github.com>
2024-04-17 01:59:56 +0530

    Update Expserver.js

--- Expserver.js
+++ Expserver.js
@@ -4,7 +4,7 @@ const app = express();
 const bodyparser = require('body-parser')
 const axios = require('axios');
 // const { title } = require('process');
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 app.use(bodyparser.urlencoded({ extended: true }))
